23f646fc81594047dc65c87656c806a1ad830d8c
Mathias Weber <mweb@gmx.ch>
2025-01-27 23:47:54 +0100

    shrink the ball to a better size and always start downwards

--- actors/ball/ball.gd
+++ actors/ball/ball.gd
@@ -5,7 +5,7 @@ var direction=Vector2.ZERO
 const MOVEMENT_SPEED: int = 375
 
 func _ready():
-	direction = Vector2(1,-1)
+	direction = Vector2(1, 1)
 
 func _physics_process(delta: float) -> void:
 	var col = move_and_collide(delta*direction*MOVEMENT_SPEED)
