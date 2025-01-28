fb48cd69fb9f0df390680a8d4ea3f3fc7be3ea8c
Mathias Weber <mweb@gmx.ch>
2025-01-28 22:38:45 +0100

    make ball collision more robust, less double collisions

--- actors/ball/ball.gd
+++ actors/ball/ball.gd
@@ -1,20 +1,27 @@
 extends AnimatableBody2D
 
 var direction=Vector2.ZERO
+var lastCollision = -1
 
 const MOVEMENT_SPEED: int = 375
+
 
 func _ready():
 	direction = Vector2(1, 1)
 
 func _physics_process(delta: float) -> void:
 	var col = move_and_collide(delta*direction*MOVEMENT_SPEED)
-	if col:
+	if col && col.get_collider_id() != lastCollision:
 		direction = direction.bounce(col.get_normal())
+		lastCollision = col.get_collider_id()
 		if col.get_collider().name == "Brick":
 			col.get_collider().get_parent().hit()
+	elif col:
+		pass
+	else:
+		lastCollision = -1
 
-	if position.y > 1000:
+	if position.y > 1080:
 		die()
 
 func die():
