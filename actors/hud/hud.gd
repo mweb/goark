420bd60443196421b1aa4e4d539a971fb0a64b87
Mathias Weber <mweb@gmx.ch>
2025-01-28 22:40:35 +0100

    add hud with score, balls and live support

---
+++ actors/hud/hud.gd
@@ -0,0 +1,23 @@
+extends Control
+
+@onready var score = $Score
+@onready var lives = $Lives
+@onready var balls = $Balls
+
+func set_score(value: int) -> void:
+	if value >= 0:
+		score.text = str(value)
+	else:
+		score.text = "INF"
+
+func set_lives(value: int) -> void:
+	if value >= 0:
+		lives.text = str(value)
+	else:
+		lives.text = "GAME OVER"
+
+func set_balls(value: int) -> void:
+	if value >= 0:
+		balls.text = str(value)
+	else:
+		balls.text = ""
