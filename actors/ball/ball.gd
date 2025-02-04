44753c7e600d1997c5b8fc7c850cb3091779dfb9
Mathias Weber <mweb@gmx.ch>
2025-02-04 23:30:52 +0100

    improved ball collision

--- actors/ball/ball.gd
+++ actors/ball/ball.gd
@@ -12,19 +12,22 @@ func _ready():
 
 func _physics_process(delta: float) -> void:
 	var col = move_and_collide(delta*direction*MOVEMENT_SPEED)
-	if col && col.get_collider_id() != lastCollision:
+	if col:
+		col.get_depth()
 		direction = direction.bounce(col.get_normal())
+		# move outside again to avoid strang bumping effects
+		move_and_collide(col.get_depth()*col.get_normal())
 		lastCollision = col.get_collider_id()
 		if col.get_collider().name == "Brick":
 			col.get_collider().get_parent().hit()
-	elif col:
-		pass
-	else:
-		lastCollision = -1
-
+		elif col.get_collider().is_class("Ball"):
+			col.get_collider().reflect(col.get_normal())
 	if position.y > 1080:
 		die()
 
+func reflect(normal: Vector2) -> void:
+	direction = direction.bounce(normal)
+
 func die():
 	queue_free()
-	emit_signal("died")
+	died.emit()
